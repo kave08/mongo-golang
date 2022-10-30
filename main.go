a2ee80539aaba7f77300293ea91baaaa4684c526
kave haj <kave.haj@snapp.cab>
2022-10-31 00:48:04 +0330

    added router

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"net/http"
+)
+
 
 func main() {
+
+	r := httprouter.New()
+	
+	r.GET("",)
+	r.POST("",)
+	r.DELETE("",)
+
 }
